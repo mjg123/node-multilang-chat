4922586ee07ffe481ab25f64ad2af2b7faeddef0
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-05-19 21:36:29 +0100

    multichat en<->es

---
+++ multichat.js
@@ -0,0 +1,46 @@
+var http = require('http');
+var https = require('https');
+var url = require('url');
+var qs = require('querystring');
+
+var messages = { "en":[], "es":[] };
+
+http.createServer(function (req, res) {
+
+  var urlParams = url.parse(req.url, true).query;
+
+  var msgLang = urlParams.lang || "en";
+
+  if ( urlParams.msg ) {
+    messages[msgLang].push( urlParams );
+
+    var targetLang = ( msgLang === "en" ? "es" : "en" );
+
+    var translateUrl = "/language/translate/v2?" +
+                       "key=AIzaSyDQvD2F99tMbspU6aA6-WWFQ1nZ1Ote0eA&"+
+                       "source=" + msgLang + "&" +
+                       "target=" + targetLang + "&" +
+                       "q=" + qs.escape(urlParams.msg);
+
+    https.get( { host: "www.googleapis.com", 
+                 path: translateUrl },
+               function ( res ){
+                  res.on( 'data', function(d){ 
+                    var translation = JSON.parse(d);
+                    var transMsg = translation.data.translations[0].translatedText;
+                    var otherMsg = { "name" : urlParams.name, "msg": transMsg }
+                    messages[targetLang].push( otherMsg );
+                } );
+               }
+             );
+
+  }
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end( (messages[msgLang]||[]).map( function(x){ return "[" + (x.name||"anonymous") + "] " + x.msg; } ).join(",\n") );
+
+
+}).listen(1337, "127.0.0.1");
+
+
+console.log('Server running at http://127.0.0.1:1337/');
